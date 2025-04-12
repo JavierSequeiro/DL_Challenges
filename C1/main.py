85f62bc4c46dfae10311bad93e4c9b963c0e5d88
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2025-04-12 21:16:18 +0200

    Main File Ready to USE =)

--- C1/main.py
+++ C1/main.py
@@ -13,8 +13,9 @@ from test_ import test_fn
 # from .test import * 
 
 # data_path = "./DL_Challenges/C1/data/15-Scene"
-data_path = "./data/15-Scene"
-cfg = Config()
+data_path = "./data/15-Scene" # Data Folder
+cfg = Config() # Experiment Config
+
 scene_dict = {0:"bedroom",
               1:"suburb",
               2:"industrial",
@@ -30,15 +31,17 @@ scene_dict = {0:"bedroom",
               12:"tall building",
               13:"office",
               14:"store"}
+
 print(sorted(os.listdir(data_path)))
 im_transform = transforms.Compose([
     transforms.Resize((128,128)),
     transforms.ToTensor(),
 ])
 
-# train_size, val_size, test_size = [0.8, 0.1, 0.1]
+# Dataset generation using torchvision
 dataset = datasets.ImageFolder(root=data_path, transform=im_transform)
 
+# Subsets random division
 dataset_size = len(dataset)
 train_size = int(cfg.train_size*dataset_size)
 val_size = int(cfg.val_size*dataset_size)
@@ -46,24 +49,27 @@ test_size = dataset_size - train_size - val_size
 
 train_set, val_set, test_set = random_split(dataset,[train_size, val_size, test_size],generator=torch.Generator().manual_seed(14))
 
-imgs, labels = val_set[70]
-print(imgs.shape)
 print(f'Train Set: {len(train_set)} Samples')
 print(f'Val Set: {len(val_set)} Samples')
 print(f'Test Set: {len(test_set)} Samples')
 
+# Generate Train, Val, Test Dataloaders
 train_loader = DataLoader(train_set, batch_size=cfg.batch_size,shuffle=True)
 val_loader = DataLoader(val_set, batch_size=cfg.batch_size, shuffle=False)
 test_loader = DataLoader(test_set, batch_size=cfg.batch_size, shuffle=False)
 
 
+# Train the model
 model, train_loss, val_loss, train_metrics, val_metrics = train_fn(train_loader=train_loader,
                                                                    val_loader=val_loader,
                                                                    cfg=cfg)
 
+# Test the model
 test_metrics = test_fn(model=model,
                        test_loader=test_loader,
                        cfg=cfg)
+
+# Plotting if needed
 
 # imgs, labels = val_set[70]
 # imgs = imgs.permute(1,2,0)
